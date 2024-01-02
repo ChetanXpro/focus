d5593c7ca122bddb4c92d1e6ea843cc55beb0af4
Chetan baliyan <chetanbaliyan10@gmail.com>
2024-01-02 16:40:04 +0530

    add dynamic path and config files

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,115 @@
-use argh::{from_env, FromArgs};
+use clap::{Args, Parser, Subcommand};
 use directories::ProjectDirs;
 use kbar::Bar;
+use serde::Serialize;
 use std::fs;
 use std::fs::OpenOptions;
 use std::io;
+use std::path::PathBuf;
+
+use std::thread::sleep;
 use std::time::Duration;
-use std::{env::args, thread::sleep};
+use toml;
+
+use toml::Value;
 
 use std::io::Write;
 
-#[derive(FromArgs)]
-/// Reach new heights.
-struct GoUp {
+// Top level struct to hold the TOML data.
+#[derive(Serialize)]
+struct Config {
+    website_list_path: String,
+}
+
+#[derive(Parser)]
+#[command(author, version)]
+#[command(
+    about = "stringer - a simple CLI to transform and inspect strings",
+    long_about = "stringer is a super fancy CLI (kidding)
+
+One can use stringer to modify or inspect strings straight from the terminal"
+)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
     /// timer for blocker
-    #[argh(option, short = 't')]
-    time: String,
-
+    #[arg(long = "time")]
+    time: Option<String>,
     /// task name
-    #[argh(option)]
-    task: String,
+    #[arg(long = "task")]
+    task: Option<String>,
 }
 
-fn is_file_exist(path: &str) -> bool {
-    return std::path::Path::new(path).is_file();
+#[derive(Subcommand)]
+enum Commands {
+    /// Reverses a string
+    Setup(Setup),
 }
 
-fn block_websites(website_list_path: &str, time_to_sleep: u64, task: &String) -> io::Result<()> {
-    let hosts_path = "/etc/hosts";
-    let backup_path: &str = "./original_host_backup";
+#[derive(Args)]
+struct Setup {
+    /// task name
+    #[arg(long = "list")]
+    list: String,
+}
 
-    let file_exist = is_file_exist(backup_path);
+// fn is_file_exist(path: &str) -> bool {
+//     return std::path::Path::new(path).is_file();
+// }
 
-    if file_exist != true {
-        fs::File::create(backup_path).expect("Error while creating hosts backup file");
+const LINUX_HOSTS_PATH: &str = "/etc/hosts";
+const MACOS_HOSTS_PATH: &str = "/etc/hosts";
+const WINDOWS_HOSTS_PATH: &str = "C:\\Windows\\System32\\drivers\\etc\\hosts";
+
+#[inline]
+const fn get_hosts_path() -> &'static str {
+    if cfg!(target_os = "linux") {
+        LINUX_HOSTS_PATH
+    } else if cfg!(target_os = "windows") {
+        WINDOWS_HOSTS_PATH
+    } else if cfg!(target_os = "macos") {
+        MACOS_HOSTS_PATH
+    } else {
+        panic!("Unsupported operating system");
+    }
+}
+
+fn block_websites(time_to_sleep: u64, task: &String) -> io::Result<()> {
+    let hosts_path: &str = get_hosts_path();
+    let mut backup_path: PathBuf = PathBuf::new();
+    let mut toml_config_path: PathBuf = PathBuf::new();
+
+    if let Some(proj_dirs) = ProjectDirs::from("com", "chetanxpro", "focusguard") {
+        let config_dir = proj_dirs.config_dir();
+
+        if !config_dir.exists() {
+            fs::create_dir_all(config_dir).expect("Error while creating config directory");
+
+            backup_path = config_dir.join("hosts_backup");
+
+            fs::File::create(&backup_path).expect("Error while creating hosts backup file");
+        }
+
+        backup_path = config_dir.join("hosts_backup");
+        toml_config_path = config_dir.join("config.toml");
+
+        dbg!(config_dir);
     }
 
-    let mut hosts_content: String =
-        fs::read_to_string(hosts_path).expect("Error while reading host file coontent");
-    let websites_list_content: String =
-        fs::read_to_string(website_list_path).expect("Error while reading website contetn");
+    let website_file_option = get_websites_path(toml_config_path);
 
+    let websites_file_path = website_file_option.as_deref().unwrap_or("default");
+
+    let mut hosts_content: String =
+        fs::read_to_string(hosts_path).expect("Error while reading host file content");
+    let websites_list_content: String =
+        fs::read_to_string(websites_file_path).expect("Error while reading website contetn");
+
+    dbg!(&backup_path);
     let mut backup_file = OpenOptions::new()
         .write(true)
         .truncate(true)
-        .open(backup_path)
+        .open(&backup_path)
         .expect("Not able to open backup file");
 
     backup_file
@@ -54,12 +120,15 @@ fn block_websites(website_list_path: &str, time_to_sleep: u64, task: &String) ->
 
     let website_list: std::str::Split<'_, &str> = websites_list_content.split("\n");
 
+    hosts_content.push_str(&format!("\n# ========== Temp Hosts ========="));
     for website in website_list {
         // println!("Website: {}",website);
+
         if !hosts_content.contains(website) {
             hosts_content.push_str(&format!("\n127.0.0.1\t{}", website));
         }
     }
+    hosts_content.push_str(&format!("\n# ========== Temp Hosts ========="));
     println!("Content:\n {}", hosts_content);
 
     let mut file = OpenOptions::new()
@@ -71,7 +140,8 @@ fn block_websites(website_list_path: &str, time_to_sleep: u64, task: &String) ->
     println!("Websites blocked");
 
     let mut bar = Bar::new();
-    bar.set_job_label(&task);
+    let format_task = format!("Timer for Task: {}", task);
+    bar.set_job_label(&format_task);
 
     for i in 0..101 {
         sleep(Duration::from_millis(time_to_sleep));
@@ -97,30 +167,80 @@ fn block_websites(website_list_path: &str, time_to_sleep: u64, task: &String) ->
     Ok(())
 }
 
+fn get_websites_path(config_path: PathBuf) -> Option<String> {
+    let config_content = fs::read_to_string(config_path).unwrap();
+    let value: Value = toml::from_str(&config_content).unwrap();
+
+    let website_list_path = value
+        .get("website_list_path")
+        .and_then(Value::as_str)
+        .map(String::from);
+    // .unwrap();
+
+    website_list_path
+}
+
 fn main() {
-    let args: GoUp = argh::from_env();
-    println!("Time: {:#?} , Task: {}", args.time, args.task);
+    let cli = Cli::parse();
 
-    let mut time_in_milliseconds: u64;
-    let time: String = args.time;
-    let task: String = args.task;
+    match &cli.command {
+        Some(Commands::Setup(setup)) => {
+            // println!("List path: {}", setup.list);
+            if let Some(proj_dirs) = ProjectDirs::from("com", "chetanxpro", "focusguard") {
+                let config_dir = proj_dirs.config_dir();
 
-    if time.contains("m") {
-        time_in_milliseconds = time.replace("m", "").parse().unwrap();
-        time_in_milliseconds = (time_in_milliseconds * 60 * 1000) / 100;
-    } else if time.contains("s") {
-        time_in_milliseconds = time.replace("s", "").parse().unwrap();
-        time_in_milliseconds = (time_in_milliseconds * 1000) / 100;
-    } else if time.contains("h") {
-        time_in_milliseconds = time.replace("h", "").parse().unwrap();
-        time_in_milliseconds = (time_in_milliseconds * 60 * 60 * 1000) / 100;
-    } else {
-        time_in_milliseconds = 0
+                if !config_dir.exists() {
+                    fs::create_dir_all(config_dir).expect("Error while creating config directory");
+
+                    fs::File::create(config_dir.join("hosts_backup"))
+                        .expect("Error while creating hosts backup file");
+                }
+
+                let config = Config {
+                    website_list_path: setup.list.clone(),
+                };
+
+                let toml = toml::to_string(&config).unwrap();
+
+                fs::File::create(config_dir.join("config.toml")).unwrap();
+
+                let mut config_file = OpenOptions::new()
+                    .write(true)
+                    .truncate(true)
+                    .open(config_dir.join("config.toml"))
+                    .unwrap();
+
+                config_file.write_all(toml.as_bytes()).unwrap();
+
+                println!("Website file path saved in config ✅")
+            }
+        }
+        None => {
+            if let (Some(time), Some(task)) = (cli.time, cli.task) {
+                // println!("lol")
+                println!("Time: {:#?} , Task: {}", time, task);
+
+                let mut time_in_milliseconds: u64;
+                // let time: String = cli.time;
+                // let task: String = args.task;
+
+                if time.contains("m") {
+                    time_in_milliseconds = time.replace("m", "").parse().unwrap();
+                    time_in_milliseconds = (time_in_milliseconds * 60 * 1000) / 100;
+                } else if time.contains("s") {
+                    time_in_milliseconds = time.replace("s", "").parse().unwrap();
+                    time_in_milliseconds = (time_in_milliseconds * 1000) / 100;
+                } else if time.contains("h") {
+                    time_in_milliseconds = time.replace("h", "").parse().unwrap();
+                    time_in_milliseconds = (time_in_milliseconds * 60 * 60 * 1000) / 100;
+                } else {
+                    time_in_milliseconds = 0
+                }
+
+                block_websites(time_in_milliseconds, &task).expect("Error")
+            } else {
+                println!("No command provided");
+            }
+        }
     }
-
-    // for _ in 0..args.time {
-    //     println!("Hello {}!", args.time)
-    // }
-    let website_list_path = "./websites.txt";
-    block_websites(website_list_path, time_in_milliseconds, &task).expect("Error")
 }
